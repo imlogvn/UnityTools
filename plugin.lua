fca64ac78723c5747f7590171c9e3892fc4f0911
Logʌn <110776616+imlogvn@users.noreply.github.com>
2024-11-18 19:27:54 -0500

    Update plugin.lua

--- plugin.lua
+++ plugin.lua
@@ -1,1 +1,9 @@
+local toolbar = plugin:CreateToolbar("Unity Tools")
+local pluginButton = toolbar:CreateButton(
+	"Test",
+	"testing",
+	"rbxassetid://8740888472")
 
+pluginButton.Click:Connect(function()
+	print("Button hass been clicked.")
+end)
